02874f7e2a9681278171c42883832ec0bfba1637
Hendrik Lüth <hendrik@linux-nerds.de>
2015-09-04 20:53:42 +0200

    make the script flexible and more efficient

--- gitpullallfffl.sh
+++ gitpullallfffl.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-cd dns_fffl && git pull && cd .. && cd dnsmasq && git pull && cd .. && cd fffl-fastd-peers && git pull && cd .. && cd Gateway-crontab && git pull && cd .. && cd lokalerGateway && git pull && cd .. && cd site-fffl && git pull && cd .. && cd snowden-backup && git pull && cd .. && cd statische_ip_zuordnung && git pull && cd .. && cd voting && git pull && cd .. && cd website && git pull && cd .. && cd voting && git pull && cd .. && cd git_pull_all_fffl_repos_script && git pull && cd .. && echo git pull fffl durchgelaufen
+for directory in *
+do
+    if [ -d $directory ]; then
+    	if [ -d $directory/.git ]; then
+            cd $directory && git pull ; cd ../
+        fi
+    fi
+done
+
