9cb6a574a250ef7569115ca95c9ac7452971425c
horstfffl <horsthorsthorst@hmamail.com>
2015-09-04 02:14:06 +0200

    einfachstes git pull script sofern die ordner schon ausgescheckt sind

---
+++ gitpullallfffl.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cd dns_fffl && git pull && cd .. && cd dnsmasq && git pull && cd .. && cd fffl-fastd-peers && git pull && cd .. && cd Gateway-crontab && git pull && cd .. && cd lokalerGateway && git pull && cd .. && cd site-fffl && git pull && cd .. && cd snowden-backup && git pull && cd .. && cd statische_ip_zuordnung && git pull && cd .. && cd voting && git pull && cd .. && cd website && git pull && cd .. && cd voting && git pull && cd .. && cd git_pull_all_fffl_repos_script && git pull && cd .. && echo git pull fffl durchgelaufen
